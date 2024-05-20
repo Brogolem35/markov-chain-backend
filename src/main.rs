8a2a1beb678921f5d6dd13c236975dbe4ed8a571
Brogolem35 <brogolem35@protonmail.com>
2024-05-20 19:48:41 +0300

    Pull PORT value from environment

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,15 @@ static GENERATOR: Lazy<MarkovChain> = Lazy::new(|| train_markov());
 
 #[tokio::main]
 async fn main() {
+    // Load variables from `.env`.
+    // It returns `Err` when `.env` is not found, but I don't always want a `.env` file to be present.
+    let _ = dotenvy::dotenv();
+
+    let port: u16 = std::env::var("PORT")
+        .expect("PORT env var")
+        .parse()
+        .expect("PORT env var couldn't parse to 16 bit unsigned int");
+
     // Training Markov Chain before starting the server. The server won't start until training is finished.
     // If this step were to be omited, the training won't begin until a POST request to `/` is sent, due to how `Lazy` is evaluated.
     {
@@ -37,7 +46,7 @@ async fn main() {
     let app = Router::new().route("/", post(generate));
 
     // Listens requests from port 3000
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("Listening at port: {}", addr.port());
